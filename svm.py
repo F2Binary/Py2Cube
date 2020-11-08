fa9694d8845c0e001dc39a2dda30230065fcfd81
f2binary <f2binary@gmail.com>
2020-11-08 15:42:34 -0800

    Finished svm model

--- svm.py
+++ svm.py
@@ -1,9 +1,26 @@
 import pandas as pd 
 import numpy as np
-from sklearn.svm import LinearSVC
+from sklearn import svm,metrics
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
-model = pd.read_csv("new_dataset.csv")
-x = df.iloc[:, [1,3,5,6]] # Will give you columns 2 and 3 i.e 'petal_length' and 'petal_width'
-y = df.iloc[:, 7] # Label column i.e 'Target'
-#X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, test_size=0.2,random_state=109
+from matplotlib import pyplot as plt
+df = pd.read_csv("new_dataset.csv")
+clf = svm.SVC(kernel='linear')
+columns = "Time Length_Scramble Length_Solution TPS".split()
+x  = df.iloc[:, [1,3,5,6]] 
+target  = df.iloc[:, 7] # Label column i.e 'Target'
+data = pd.DataFrame(x,columns=columns)
+y = target
+X_train, X_test, y_train, y_test = train_test_split(data, y, test_size=0.3)
+#print(X_train.shape,y_train.shape)
+#print(X_test.shape, y_test.shape)
+clf.fit(X_train,y_train)
+y_pred = clf.predict(X_test)
+predictions = clf.predict(X_test)
+#plt.scatter(y_test,predictions)
+#plt.xlabel("True Values")
+#plt.ylabel("Predictions")
+#plt.show()
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Precision:",metrics.precision_score(y_test, y_pred))
+print("Recall:",metrics.recall_score(y_test, y_pred))
